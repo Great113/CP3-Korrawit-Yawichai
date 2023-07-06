068b1d60c998b1b3c4573e7633ef05f5f19091ec
Great113 <greater2318@gmail.com>
2023-07-06 22:31:15 +0700

    Create Exercise4_Korrawit_Y.py

---
+++ Exercise4_Korrawit_Y.py
@@ -0,0 +1,9 @@
+x = 90.0
+y = 75.3
+z = 100.0
+c = 85.7
+print("Your Score :) ")
+print("Foundation English :",x)
+print("General Business :",y)
+print("Introducion to Computer Systems :",z)
+print("Computer Programming :",c)
