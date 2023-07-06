7a4ab042f980e17e73b933a195928a3578cb4bc4
Great113 <greater2318@gmail.com>
2023-07-06 22:34:35 +0700

    Update Exercise4_Korrawit_Y.py

--- Exercise4_Korrawit_Y.py
+++ Exercise4_Korrawit_Y.py
@@ -1,9 +1,9 @@
-x = 90.0
-y = 75.3
-z = 100.0
-c = 85.7
+Eng = 90.0
+Business = 75.3
+IntroCom = 100.0
+ComProramming = 85.7
 print("Your Score :) ")
-print("Foundation English :",x)
-print("General Business :",y)
-print("Introducion to Computer Systems :",z)
-print("Computer Programming :",c)
+print("Foundation English :",Eng)
+print("General Business :",Business)
+print("Introducion to Computer Systems :",IntroCom)
+print("Computer Programming :",ComProramming)
