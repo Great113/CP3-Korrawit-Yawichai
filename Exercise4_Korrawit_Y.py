2882cb60e2db5abe49d45c2983fc4dd02b47e59c
Great113 <greater2318@gmail.com>
2023-07-08 13:11:43 +0700

    add line

--- Exercise4_Korrawit_Y.py
+++ Exercise4_Korrawit_Y.py
@@ -3,7 +3,9 @@ Business = 75.3
 IntroCom = 100.0
 ComProramming = 85.7
 print("Your Score :) ")
+print("------------------")
 print("Foundation English :",Eng)
 print("General Business :",Business)
 print("Introducion to Computer Systems :",IntroCom)
 print("Computer Programming :",ComProramming)
+print("------------------")
